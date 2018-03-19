2e881d188d1e3eb047deab29392087ec753edfe0
natbarroca <37390996+natbarroca@users.noreply.github.com>
2018-03-18 22:39:20 -0300

    Update cost_controller.rb
    Ajuste

--- app/controllers/cost_controller.rb
+++ app/controllers/cost_controller.rb
@@ -5,30 +5,7 @@ class CostController < ApplicationController
 
   	require 'dijkstraclass'
 
-    #r = Shipping.pluck(:origin,:destination, :distance)
-
-    r = [
-	 ["A", "B", 1],
-     ["A", "F", 9],
-     ["D", "J", 3],
-     ["G", "C", 3],
-     ["A", "C", 7],
-     ["F", "B", 2],
-     ["B", "C", 1],
-     ["C", "F", 4],
-     ["E", "Z", 2]]
-
-     r2 = [[1, 3, 9],
-     [1, 5, 3],
-     [2, 4, 3],
-     [2, 3, 7],
-     [4, 3, 2],
-     [4, 1, 1],
-     [5, 2, 4],
-     [5, 4, 2]]
-
-     #ojbteste = Test.new("1", "3")
-
+    r = Shipping.pluck(:origin,:destination, :distance)
 
     start_point = cost_params["origin"] # starting node
 	end_point = cost_params["destination"] # arrival node
@@ -36,8 +13,6 @@ class CostController < ApplicationController
 	puts 'chamando método'
 
 	obj = DijkstraClass.new(start_point, end_point, r)
-
-	#ob = Dijkstra.new("A", "B", r3)
 
 	@cost = obj.cost
 
